67a146d89cfc7b12aa445955296bd7c2570bb2a7
Jason <jmpanugaling@gmail.com>
2016-02-01 12:27:54 +0800

    Added Applicant update request

---
+++ app/Http/Requests/UpdateApplicantsRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateApplicantsRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'fullname'      =>  'required | max:255',
+            'position'      =>  'required | max:255',
+            'email'         =>  'required | max:255',
+            'contact'       =>  'required | max:255',
+            'location'      =>  'required',
+            'skills'        =>  'required'
+        ];
+    }
+}
